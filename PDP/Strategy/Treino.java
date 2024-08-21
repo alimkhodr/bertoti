377dcd8b5f84211a50fb3840289a37f32e7f3d81
Ali Mohamed <85517447+alimkhodr@users.noreply.github.com>
2024-08-20 21:59:23 -0300

    Add files via upload

---
+++ PDP/Strategy/Treino.java
@@ -0,0 +1,47 @@
+public interface Treino {
+    void treinar();
+}
+
+public class TreinoGerente implements Treino {
+    @Override
+    public void treinar() {
+        System.out.println("Treinamento específico para Gerente.");
+    }
+}
+
+public class TreinoSupervisor implements Treino {
+    @Override
+    public void treinar() {
+        System.out.println("Treinamento específico para Supervisor.");
+    }
+}
+
+public class Funcionario {
+    private Treino treino;
+
+    public void setTreino(Treino treino) {
+        this.treino = treino;
+    }
+
+    public void aplicarTreinamento() {
+        if (treino != null) {
+            treino.treinar();
+        } else {
+            System.out.println("Nenhum treino definido.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+
+        // Gerente
+        Treino treinoGerente = new TreinoGerente();
+        funcionario.setTreino(treinoGerente);
+        funcionario.aplicarTreinamento();
+
+        // Supervisor
+        Treino treinoSupervisor = new TreinoSupervisor();
+        funcionario.setTreino(treinoSupervisor);
+        funcionario.aplicarTreinamento();
+    }
+}
